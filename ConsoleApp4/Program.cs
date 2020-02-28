4cf0f62ae03df546b38c4ab858023c4f36b16106
Daniya Bekzhan <Marozhkakim25@gmail.com>
2020-02-28 14:55:04 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Person : IPrint
+    {
+        public void print()
+        {
+            Console.WriteLine("It's a persoooon!!!");
+        }
+
+        class Teacher : IPrint
+        {
+            public void print()
+            {
+                Console.WriteLine("It's a teacheeeer!!!");
+            }
+        }
+
+        class Student : IPrint, ISred
+        {
+            public int[] marks = { 5, 4, 5, 3, 2 };
+            public void print()
+            {
+                Console.WriteLine("It's a studeeeeent!!!");
+            }
+
+
+            public int calculate()
+            {
+                int summa = 0;
+                int count = 0;
+                for (int a = 0; a < marks.Length; a++)
+                {
+                    summa = summa + marks[a];
+                    count++;
+                }
+                return summa / count;
+            }
+        }
+
+        interface IPrint
+        {
+            void print();
+        }
+
+        interface ISred
+        {
+            int calculate();
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
+}
